4a7f801ab2596bae430b65369665cc22feee1e34
Lennart Hase <lenny.glk@gmail.com>
2019-06-26 00:50:05 +0100

    Added some quick tests and randomised sequence and timestamp for voice
    Signed-off-by: Lennart Hase <lenny.glk@gmail.com>

---
+++ src/tests/DiscordClient.test.ts
@@ -0,0 +1,46 @@
+import Logger from '../common/Logger';
+import DiscordManager from '../rest/DiscordManager';
+import ChannelStore from '../stores/ChannelStore';
+import GuildStore from '../stores/GuildStore';
+import VoiceStateStore from '../stores/VoiceStateStore';
+import DiscordClient from './../DiscordClient';
+
+
+describe('Users', () => {
+  let instance: DiscordClient;
+
+  beforeEach(() => {
+    instance = new DiscordClient({token: "DISCORD_TOKEN"})
+  });
+
+  it('should set token property', () => {
+    expect(instance).toBeInstanceOf(DiscordClient);
+    expect(instance.token).toBe("DISCORD_TOKEN");
+  });
+  
+  it('should create a new logger instance', () => {
+    expect(instance).toBeInstanceOf(DiscordClient);
+    expect(instance.logger).toBeInstanceOf(Logger);
+  });
+
+  it('should create a new Channels property - ChannelStore instance', () => {
+    expect(instance).toBeInstanceOf(DiscordClient);
+    expect(instance.Channels).toBeInstanceOf(ChannelStore);
+  });
+
+  it('should create a new Guilds property - GuildStore instance', () => {
+    expect(instance).toBeInstanceOf(DiscordClient);
+    expect(instance.Guilds).toBeInstanceOf(GuildStore);
+  });
+
+  it('should create a new VoiceStates property - VoiceStateStore instance', () => {
+    expect(instance).toBeInstanceOf(DiscordClient);
+    expect(instance.VoiceStates).toBeInstanceOf(VoiceStateStore);
+  });
+
+  it('should create a new DiscordAPIManager property - DiscordManager instance', () => {
+    expect(instance).toBeInstanceOf(DiscordClient);
+    expect(instance.DiscordAPIManager).toBeInstanceOf(DiscordManager);
+  });
+  
+});
